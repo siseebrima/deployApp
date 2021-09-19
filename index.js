91e24d2d3fb55b44c92d60e6f36cd4cd5c8e75ef
Ebrima Sise <siseebrima@gmail.com>
2021-09-19 19:50:12 +0000

    add the second pipe

--- index.js
+++ index.js
@@ -81,7 +81,7 @@ app.post("/api/notes", (request, response) => {
   response.json(note);
 });
 
-const PORT = process.env.PORT | 3001;
+const PORT = process.env.PORT || 3001;
 app.listen(PORT, () => {
   console.log(`Ebrima, the server is listening on port ${PORT}`);
 });
