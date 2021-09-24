8acff287538933614a110336c1881155b5099ff5
Ebrima Sise <siseebrima@gmail.com>
2021-09-24 13:16:42 +0000

    uibuild

--- index.js
+++ index.js
@@ -1,39 +1,57 @@
 import express from "express";
 // import http from "http";
 import cors from "cors";
+import mongoose from "mongoose";
 
 const app = express();
 app.use(express.json());
 app.use(cors());
 app.use(express.static("build"));
 
-let notes = [
-  {
-    id: 1,
-    content: "HTML is easy",
-    date: "2019-05-30T17:30:31.098Z",
-    important: true,
-  },
-  {
-    id: 2,
-    content: "Browser can execute only Javascript",
-    date: "2019-05-30T18:39:34.091Z",
-    important: false,
-  },
-  {
-    id: 3,
-    content: "GET and POST are the most important methods of HTTP protocol",
-    date: "2019-05-30T19:20:14.298Z",
-    important: true,
-  },
-];
+// let notes = [
+//   {
+//     id: 1,
+//     content: "HTML is easy",
+//     date: "2019-05-30T17:30:31.098Z",
+//     important: true,
+//   },
+//   {
+//     id: 2,
+//     content: "Browser can execute only Javascript",
+//     date: "2019-05-30T18:39:34.091Z",
+//     important: false,
+//   },
+//   {
+//     id: 3,
+//     content: "GET and POST are the most important methods of HTTP protocol",
+//     date: "2019-05-30T19:20:14.298Z",
+//     important: true,
+//   },
+// ];
+
+const url = `mongodb+srv://ebrima:quooch8O@cluster0.cdfzb.mongodb.net/note-app?retryWrites=true&w=majority`;
+
+mongoose.connect(url).then((res) => {
+  console.log(`connected with no problem`);
+});
+
+const noteSchema = new mongoose.Schema({
+  content: String,
+  date: Date,
+  important: Boolean,
+});
+
+const Note = new mongoose.model("Note", noteSchema);
 
 app.get("/", (req, res) => {
   res.send("<h1>Hello Ebrima</h1>");
 });
 
 app.get("/api/notes", (req, res) => {
-  res.json(notes);
+  Note.find({}).then((notes) => {
+    res.json(notes);
+    mongoose.connection.close();
+  });
 });
 
 app.get("/api/notes/:id", (request, response) => {
@@ -72,14 +90,18 @@ app.post("/api/notes", (request, response) => {
     });
   }
 
-  const note = {
+  const note = new Note({
     content: body.content,
     important: body.important || false,
     date: new Date(),
     id: generateId(),
-  };
-  notes = notes.concat(note);
-  response.json(note);
+  });
+  // notes = notes.concat(note);
+  // response.json(note);
+  note.save().then((result) => {
+    console.log(`the note is successfully saved`);
+    mongoose.connection.close();
+  });
 });
 
 const PORT = process.env.PORT || 3001;
